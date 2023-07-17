d428c44643e2c611272dbe06eaea2aa57b3d9e88
Omar Ahmed <omar52ramos@gmail.com>
2023-07-17 12:31:54 -0600

    added encryption template

---
+++ Portfolio Assignment/2/encryption_template.py
@@ -0,0 +1,18 @@
+# encryption.py
+# STUDENT NAME, ENDG 233 F21
+# A terminal-based encryption application capable of both encoding and decoding text when given a specific cipher.
+# You may optionally import the string module from the standard Python library. No other modules may be imported.
+# Remember to include docstrings for your functions and comments throughout your code.
+
+
+
+
+### Define your functions here
+
+
+print("ENDG 233 Encryption Program")
+
+### Add your main program code here
+
+print('Thank you for using the encryption program.')
+
