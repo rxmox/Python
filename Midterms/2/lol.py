a3f64b0c8a3bb9bcd4e19a1e4770fc147885578e
Omar Ahmed <omar52ramos@gmail.com>
2023-08-15 16:15:39 -0600

    added remaining midterm files

---
+++ Midterms/2/lol.py
@@ -0,0 +1,7 @@
+char_count = 0
+user_text = ('Gay A!Xel,')
+for character in user_text:
+    if (character != ' ') or (character != '.') or (character != ',') or (character != '!'):
+        char_count +=1
+
+print(char_count)
